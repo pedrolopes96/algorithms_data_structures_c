af2e3d5fd0c8bc53adee91a507764a8e9fd5f0b3
Pedro <pedroflopes1710@gmail.com>
2023-01-29 22:30:37 -0300

    ler e escrever um número inteiro

---
+++ S3Ex001.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int numero;
+    printf("Informe um numero: ");
+    scanf("%d", &numero);
+    printf("O numero informado foi: %d", numero);
+    return 0;
+}
